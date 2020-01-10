2efa886ea652a95e53ad018b8fb909361d4cc9e9
Benjamin <>
2020-01-10 15:42:52 +0300

    new build from the new PC

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,8 +1,8 @@
 
 import java.rmi.RemoteException;
 import javax.xml.rpc.ServiceException;
+import static junit.framework.Assert.assertEquals;
 import org.ibm.ws.OCSWebMethods;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestClass {
@@ -11,6 +11,6 @@ public class TestClass {
     public void test() throws ServiceException, RemoteException {
         OCSWebMethods ocsWebMethods = new OCSWebMethods("172.27.122.45", "9002");
 
-        assertEquals("405000000", ocsWebMethods.queryBalance("753001184", "VOL-CHECK").getResultHeader().getResultCode());
+        assertEquals("405000000", ocsWebMethods.queryBalance("750376961", "VOL-CHECK").getResultHeader().getResultCode());
     }
 }
